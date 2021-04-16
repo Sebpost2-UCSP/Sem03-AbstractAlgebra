402c91074d3ad66dcd4c2f8d4b4095f9b6a06590
sebpost2 <81270882+sebpost2@users.noreply.github.com>
2021-04-16 12:11:08 -0500

    Update practica parte 2.cpp

--- practica parte 2.cpp
+++ practica parte 2.cpp
@@ -4,12 +4,12 @@
 int main()
 {
     std::string A;
-    int B=202;
+    int B;
     std::cout << "Ingrese un numero\n";
     std::cin >> A;
     std::stringstream change;
-    change >> A;
-    change << B;
+    change << A;
+    change >> B;
     std::cout<<"\n";
     std::cout << A;
 }
